d55c8abb1f23cdd806ae1856915f1da40e57f89f
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-05-19 13:09:26 +1000

    Refactoring constraints

--- ImageScroll/ImageScrollViewController.m
+++ ImageScroll/ImageScrollViewController.m
@@ -10,75 +10,74 @@
 
 @interface ImageScrollViewController () <UIScrollViewDelegate>
 
-@property (strong, nonatomic) UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation ImageScrollViewController
 
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
-    return self.imageView;
-}
-
 - (void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
     self.scrollView.delegate = self;
-    [self createImageView];
-    [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.view setNeedsUpdateConstraints];
+    [self initImageView];
+    [self initZoom];
 }
 
-- (void) createImageView {
-    UIImage *image =  [UIImage imageNamed:@"wallabi.jpg"];
-    self.imageView = [[UIImageView alloc] initWithImage:image];
-    [self.imageView setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.scrollView addSubview:self.imageView];
-    self.scrollView.contentSize = image.size;
-}
-
-- (void) updateViewConstraints {
-    [super updateViewConstraints];
-    NSLog(@"image view size: %@", NSStringFromCGSize(self.imageView.bounds.size));
-    NSLog(@"scroll view size: %@", NSStringFromCGSize(self.scrollView.bounds.size));
-    NSLog(@"Scroll View content size: %@", NSStringFromCGSize(self.scrollView.contentSize));
-
-    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
-    NSLog(@"Number of scrollView consraints: %i", self.scrollView.constraints.count);
-    NSLog(@"Number of imageView consraints: %i", self.imageView.constraints.count);
-}
-
-//- (void) viewWillAppear:(BOOL)animated {
-//    [super viewWillAppear:animated];
-//
-//    NSLog(@"----------- viewWillAppear START -----------");
-//    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
-//    NSLog(@"Number of imageView consraings: %i", self.imageView.constraints.count);
-//
-//    [self.view removeConstraints:[self.view constraints]];
-//    [self.imageView removeConstraints:[self.imageView constraints]];
-//    
-//    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
-//    NSLog(@"Number of imageView consraings: %i", self.imageView.constraints.count);
-//    
-//    
-//    [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
-//    [self.imageView setTranslatesAutoresizingMaskIntoConstraints:NO];
+- (void) initImageView {
+    [self.scrollView removeConstraints: self.scrollView.constraints];
     
-//    NSString *imageViewConstraintH = [NSString stringWithFormat:@"|-[_imageView(%.f)]-|", self.imageView.image.size.width ];
-//    
-//    NSString *imageViewConstraintV = [NSString stringWithFormat:@"V:|-[_imageView(%.f)]-|", self.imageView.image.size.height ];
-//
-//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:imageViewConstraintH options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
-//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:imageViewConstraintV options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-(>=0@750)-[_imageView]-(>=0@250)-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0@750)-[_imageView]-(>=0@250)-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
+    
+    [self addConstraintsToCenterImage];
+}
 
-//    [self.imageView setNeedsUpdateConstraints];
-//    [self.view setNeedsUpdateConstraints];
-//    
-//    NSLog(@"----------- viewWillAppear END -----------");
-//    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
-//    NSLog(@"Number of imageView consraings: %i", self.imageView.constraints.count);
-//}
+- (void) addConstraintsToCenterImage {
+    NSLayoutConstraint *constraintMiddleY =
+    [NSLayoutConstraint constraintWithItem:self.imageView
+                                 attribute:NSLayoutAttributeCenterY
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.scrollView
+                                 attribute:NSLayoutAttributeCenterY
+                                multiplier:1.0
+                                  constant:0.0];
+    constraintMiddleY.priority = 100;
+    [self.view addConstraint:constraintMiddleY];
+    
+    NSLayoutConstraint *constraintMiddleX =
+    [NSLayoutConstraint constraintWithItem:self.imageView
+                                 attribute:NSLayoutAttributeCenterX
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.scrollView
+                                 attribute:NSLayoutAttributeCenterX
+                                multiplier:1.0
+                                  constant:0.0];
+    constraintMiddleX.priority = 100;
+    [self.view addConstraint:constraintMiddleX];
+}
+
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [self initZoom];
+}
+
+// Zoom to show as much image as possible
+- (void) initZoom {
+    float minZoom = MIN(self.view.bounds.size.width / self.imageView.image.size.width,
+                        self.view.bounds.size.height / self.imageView.image.size.height);
+    if (minZoom > 1) return;
+    
+    self.scrollView.minimumZoomScale = minZoom;
+    
+    self.scrollView.zoomScale = minZoom;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
 
 @end
