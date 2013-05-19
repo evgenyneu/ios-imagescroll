61e83f68f112953eae01da3a57d6be10dd953ce2
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-05-19 12:11:23 +1000

    Experimenting with auto layout

--- ImageScroll/ImageScrollViewController.m
+++ ImageScroll/ImageScrollViewController.m
@@ -8,22 +8,92 @@
 
 #import "ImageScrollViewController.h"
 
-@interface ImageScrollViewController ()
+@interface ImageScrollViewController () <UIScrollViewDelegate>
+
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIScrollView *scrollView;
+
 
 @end
 
 @implementation ImageScrollViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self createScrollView];
+    [self createImageView];
+    [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.view setNeedsUpdateConstraints];
 }
+
+- (void) createScrollView {
+    self.scrollView = [[UIScrollView alloc] init];
+    self.scrollView.delegate = self;
+    self.scrollView.maximumZoomScale = 3;
+    self.scrollView.minimumZoomScale = 0.2;
+    
+    [self.scrollView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self.view addSubview:self.scrollView];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_scrollView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_scrollView)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_scrollView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_scrollView)]];
+}
+
+- (void) createImageView {
+    UIImage *image =  [UIImage imageNamed:@"wallabi.jpg"];
+    self.imageView = [[UIImageView alloc] initWithImage:image];
+    [self.imageView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.scrollView addSubview:self.imageView];
+    self.scrollView.contentSize = image.size;
+}
+
+- (void) updateViewConstraints {
+    [super updateViewConstraints];
+    NSLog(@"image view size: %@", NSStringFromCGSize(self.imageView.bounds.size));
+    NSLog(@"scroll view size: %@", NSStringFromCGSize(self.scrollView.bounds.size));
+    NSLog(@"Scroll View content size: %@", NSStringFromCGSize(self.scrollView.contentSize));
+
+    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
+    NSLog(@"Number of scrollView consraints: %i", self.scrollView.constraints.count);
+    NSLog(@"Number of imageView consraints: %i", self.imageView.constraints.count);
+}
+
+//- (void) viewWillAppear:(BOOL)animated {
+//    [super viewWillAppear:animated];
+//
+//    NSLog(@"----------- viewWillAppear START -----------");
+//    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
+//    NSLog(@"Number of imageView consraings: %i", self.imageView.constraints.count);
+//
+//    [self.view removeConstraints:[self.view constraints]];
+//    [self.imageView removeConstraints:[self.imageView constraints]];
+//    
+//    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
+//    NSLog(@"Number of imageView consraings: %i", self.imageView.constraints.count);
+//    
+//    
+//    [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
+//    [self.imageView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+//    NSString *imageViewConstraintH = [NSString stringWithFormat:@"|-[_imageView(%.f)]-|", self.imageView.image.size.width ];
+//    
+//    NSString *imageViewConstraintV = [NSString stringWithFormat:@"V:|-[_imageView(%.f)]-|", self.imageView.image.size.height ];
+//
+//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:imageViewConstraintH options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
+//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:imageViewConstraintV options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
+
+//    [self.imageView setNeedsUpdateConstraints];
+//    [self.view setNeedsUpdateConstraints];
+//    
+//    NSLog(@"----------- viewWillAppear END -----------");
+//    NSLog(@"Number of view consraings: %i", self.view.constraints.count);
+//    NSLog(@"Number of imageView consraings: %i", self.imageView.constraints.count);
+//}
 
 @end
