14641d9745a263b452432c8a50343a29d76d6aaa
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-06-16 16:54:36 +1000

    Remove code

--- ImageScroll/ImageScrollViewController.m
+++ ImageScroll/ImageScrollViewController.m
@@ -22,52 +22,8 @@
     [super viewWillAppear:animated];
     
     self.scrollView.delegate = self;
-//    [self initImageView];
     [self initZoom];
 }
-
-- (void) initImageView {
-    [self.scrollView removeConstraints: self.scrollView.constraints];
-    
-    [self addConstraintsForImageEdges];
-    
-    [self addConstraintsToCenterImage];
-}
-
-/*
- Three things to note here:
- 1) Constraints are added to self.view and not to self.scrollView. Otherwise the picture will jump when zooming.
- 2) Notice that top and left edges have priority 750, and right and bottom have 250.
- 3) Using '>=' instead of equality to allow centering in addConstraintsToCenterImage.*/
-- (void) addConstraintsForImageEdges {
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-(>=0@750)-[_imageView]-(>=0@250)-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0@750)-[_imageView]-(>=0@250)-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_imageView)]];
-}
-
-- (void) addConstraintsToCenterImage {
-    NSLayoutConstraint *constraintMiddleY =
-    [NSLayoutConstraint constraintWithItem:self.imageView
-                                 attribute:NSLayoutAttributeCenterY
-                                 relatedBy:NSLayoutRelationEqual
-                                    toItem:self.scrollView
-                                 attribute:NSLayoutAttributeCenterY
-                                multiplier:1.0
-                                  constant:0.0];
-    constraintMiddleY.priority = 100;
-    [self.view addConstraint:constraintMiddleY];
-    
-    NSLayoutConstraint *constraintMiddleX =
-    [NSLayoutConstraint constraintWithItem:self.imageView
-                                 attribute:NSLayoutAttributeCenterX
-                                 relatedBy:NSLayoutRelationEqual
-                                    toItem:self.scrollView
-                                 attribute:NSLayoutAttributeCenterX
-                                multiplier:1.0
-                                  constant:0.0];
-    constraintMiddleX.priority = 100;
-    [self.view addConstraint:constraintMiddleX];
-}
-
 
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
 {
